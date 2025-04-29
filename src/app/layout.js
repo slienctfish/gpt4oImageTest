e5dad5b6b4d58d1c490f75b79271995c9dbce4e9
lijianhao <eclijianhao@163.com>
2025-04-29 13:55:40 +0800

    添加GA分析代码

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,13 +7,27 @@ const inter = Inter({
 });
 
 export const metadata = {
-  title: "一句话调用AI生成图片",
-  description: "一句话调用AI生成图片",
+  title: "AI Image Generator",
+  description: "Generate images using AI",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-BSZK6742NK"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-BSZK6742NK');
+            `,
+          }}
+        />
+      </head>
       <body className={`${inter.variable} antialiased`}>
         {children}
       </body>
